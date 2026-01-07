bb1459378275e19789a5dc766025ffa95065edcf
Andrew Plutto <pluttan@ya.ru>
2026-01-07 03:11:22 +0300

    chore: bump to v2.0.16

--- homebrew/vol.rb
+++ homebrew/vol.rb
@@ -6,8 +6,35 @@ class Vol < Formula
   license "MIT"
   version "2.0.0"
 
+  resource "zsh_completion" do
+    url "https://github.com/pluttan/volumes/releases/download/v2.0.0/_vol"
+    sha256 "PLACEHOLDER_ZSH_SHA256"
+  end
+
+  resource "bash_completion" do
+    url "https://github.com/pluttan/volumes/releases/download/v2.0.0/vol.bash"
+    sha256 "PLACEHOLDER_BASH_SHA256"
+  end
+
+  resource "fish_completion" do
+    url "https://github.com/pluttan/volumes/releases/download/v2.0.0/vol.fish"
+    sha256 "PLACEHOLDER_FISH_SHA256"
+  end
+
   def install
     bin.install "vol"
+    
+    resource("zsh_completion").stage do
+      zsh_completion.install "_vol"
+    end
+    
+    resource("bash_completion").stage do
+      bash_completion.install "vol.bash" => "vol"
+    end
+    
+    resource("fish_completion").stage do
+      fish_completion.install "vol.fish"
+    end
   end
 
   test do
--- vol/cli.py
+++ vol/cli.py
@@ -87,7 +87,7 @@ Examples:
     parser.add_argument("-c", "--config", default="vol.toml", help="Config file (default: vol.toml)")
     parser.add_argument("-l", "--list", action="store_true", help="List all tasks")
     parser.add_argument("--completion", action="store_true", help=argparse.SUPPRESS)
-    parser.add_argument("-v", "--version", action="version", version="vol 2.0.14")
+    parser.add_argument("-v", "--version", action="version", version="vol 2.0.16")
     
     # Use parse_known_args to allow passing extra args to commands (e.g. VERSION=2.0.0)
     args, extra_args = parser.parse_known_args()
